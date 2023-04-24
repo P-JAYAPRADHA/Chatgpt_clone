9f733e1884622dcc0f4077b4649fbbdf47681910
P-JAYAPRADHA <pjayapradha303@gmail.com>
2023-04-24 15:26:55 +0530

    Add files via upload

---
+++ chatgpt_clone/chatgpt_app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+
+openai.api_key = "<your api key from chatgpt>"
+
+def chat_view(request):
+    if request.method == 'POST':
+        user_input = request.POST['user_input']
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": user_input},
+            ],
+            temperature=0.5,
+            max_tokens=150,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            n=1,
+            stop=["\nUser:"],
+        )
+
+        bot_response = response["choices"][0]["message"]["content"]
+        return JsonResponse({'response': bot_response})
+
+    return render(request, 'chat.html')
